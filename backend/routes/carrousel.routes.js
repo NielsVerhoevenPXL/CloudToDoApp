01253ab083f61ee00fd9c3fb6c2224b9380cdc5b
NielsVerhoevenPXL <93255062+NielsVerhoevenPXL@users.noreply.github.com>
2024-11-07 23:18:41 +0100

    Update carrousel.routes.js

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -17,7 +17,7 @@ async function getImageUrlsFromS3Bucket() {
         const images = data.Contents
             .filter(item => item.Size > 0 && item.Key.match(/\.(jpeg|jpg|gif|png)$/i)) 
             .map(item => {
-                const imageUrl = `https://{{bucket_name}}.s3.amazonaws.com/${item.Key}`; 
+                const imageUrl = `https://{{bucket_name}}.s3.us-east-1.amazonaws.com/${item.Key}`; 
                 return { url: imageUrl };
             });
         console.log('Filtered images:', images); // Log the final image array to check URLs
