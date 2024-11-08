0f3d4b38af37f793298b4750a3879114a2e2900a
NielsVerhoevenPXL <93255062+NielsVerhoevenPXL@users.noreply.github.com>
2024-11-08 12:54:49 +0100

    Update carrousel.routes.js

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -4,37 +4,41 @@ const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3();
 
-async function getImageUrlsFromS3Bucket() {
-    const params = {
-        Bucket: 'automation-pe-2-cumbucket',
-        Prefix: 'pictures/'
-    };
+async function getImagePreSignedUrls() {
+  const params = {
+    Bucket: 'automation-pe-2-cumbucket',
+    Prefix: 'pictures/',
+    Expires: 60, // Set the expiration time (in seconds) for the pre-signed URLs
+  };
 
-    try {
-        console.log('Fetching images from S3 bucket...');
-        const data = await s3.listObjectsV2(params).promise();
-        console.log('Raw data from S3:', data); // Log the full data to inspect Contents
-        const images = data.Contents
-            .filter(item => item.Size > 0 && item.Key.match(/\.(jpeg|jpg|gif|png)$/i)) 
-            .map(item => {
-                const imageUrl = `https://automation-pe-2-cumbucket.s3.amazonaws.com/${item.Key}`; 
-                return { url: imageUrl };
-            });
-        console.log('Filtered images:', images); // Log the final image array to check URLs
-        return images;
-    } catch (error) {
-        console.error('Error fetching images from S3 bucket:', error);
-        return [];
-    }
+  try {
+    console.log('Fetching pre-signed URLs from S3 bucket...');
+    const data = await s3.listObjectsV2(params).promise();
+    const preSignedUrls = await Promise.all(
+      data.Contents.filter((item) => item.Size > 0 && item.Key.match(/\.(jpeg|jpg|gif|png)$/i)).map(async (item) => {
+        const url = await s3.getSignedUrlPromise('getObject', {
+          Bucket: 'automation-pe-2-cumbucket',
+          Key: item.Key,
+          Expires: params.Expires,
+        });
+        return { url };
+      })
+    );
+    console.log('Pre-signed URLs:', preSignedUrls);
+    return preSignedUrls;
+  } catch (error) {
+    console.error('Error fetching pre-signed URLs from S3 bucket:', error);
+    return [];
+  }
 }
 
 carrouselRouter.get('', async (req, res) => {
-    try {
-        const imageUrls = await getImageUrlsFromS3Bucket();
-        res.json(imageUrls);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const preSignedUrls = await getImagePreSignedUrls();
+    res.json(preSignedUrls);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = carrouselRouter;
