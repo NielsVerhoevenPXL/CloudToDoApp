63354d4b4aa4a26e6fb776eb7d15dfb5c99fc335
niels <niels851@hotmail.com>
2024-11-08 15:47:41 +0100

    s

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -5,6 +5,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
     accessKeyId: "ASIAU5PBNJM7FAEJODV2",
     secretAccessKey: "WoZTmKl9xwI01YHlw0jJy",
+    sessionToken: "IQoJb3JpZ2luX2VjENH//////////wEaCXVzLXdlc3QtMiJHMEUCIQDaAFpJ5Gy9qb+OfnoG7cXjUkZW+ifOHcmufXtnynvmlAIgJH0RW4Z7VvGrlGwp2Jn489IeHN/71XeCPs0NoHaMvFMquAIIWhABGgwzMzgxNjQ1MzQwNzgiDEKH/+kGo3sJZAhqKCqVAu06UOf5x6Z+duV7WRhmbPqtlQsfQD2fzCZlDAQuHDMmyRIiiNDOIDAuc0L4T+C6HfOpmX/cVBRCGpHZi10tharZaDxADUqtKDg0H1imYA5XJZbxf1glQXdgtH/sGBdBEyHg7BkIIRjNdqrvo3tKaBE7idUmjLPI8sp8LD61e+lvahDOfNAZ57WRI6FwpRChKZ2Fz6dRSm+6EpJWXkDWOx4ta5YiXcfZsE+aD6wyHN3aoPyqZtw0nh7LcdK6VShPsDPliC4fx99DccjxtgZzpQVugyMiufiu4kMn3iXu+TWcVtcDj+wFQpssvzVw1NNXk/c/Zv4AZ03ddE0r05MN73d4RXBxDn7C3cuQhu6bsvDv+AOGGDAwvZS3uQY6nQEVqYvwPW3Umwy4FVcruxpnclCO23ysoa5DeH8v9jjYMG6Pax+4V2cIDbsC9q/8zEodzoCGZVx1ihBht4E5k3vHJeMwSqHadqOf2NiedCNmrknUIJOUxYnAByG1PCMyAGozRv9B0jUd8YnStQo0OeXYjtCLZjGQM3Pdygg5s8cmGZbmmkhTstcbT9tp+wmBbE/1fYAQfuK8RcX5iekh",
     region: 'us-east-1',
 });
 
