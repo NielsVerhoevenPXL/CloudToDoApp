42b8313ed71427a4855321c8e6e0708705cc9017
niels <niels851@hotmail.com>
2024-11-08 13:13:52 +0100

    d

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,44 +1,26 @@
 const express = require('express');
-const carrouselRouter = express.Router();
 const AWS = require('aws-sdk');
+const carrouselRouter = express.Router();
 
 const s3 = new AWS.S3();
 
-async function getImagePreSignedUrls() {
-  const params = {
-    Bucket: 'automation-pe-2-cumbucket',
-    Prefix: 'pictures/',
-    Expires: 60, // Set the expiration time (in seconds) for the pre-signed URLs
-  };
-
-  try {
-    console.log('Fetching pre-signed URLs from S3 bucket...');
-    const data = await s3.listObjectsV2(params).promise();
-    const preSignedUrls = await Promise.all(
-      data.Contents.filter((item) => item.Size > 0 && item.Key.match(/\.(jpeg|jpg|gif|png)$/i)).map(async (item) => {
-        const url = await s3.getSignedUrlPromise('getObject', {
-          Bucket: 'automation-pe-2-cumbucket',
-          Key: item.Key,
-          Expires: params.Expires,
-        });
-        return { url };
-      })
-    );
-    console.log('Pre-signed URLs:', preSignedUrls);
-    return preSignedUrls;
-  } catch (error) {
-    console.error('Error fetching pre-signed URLs from S3 bucket:', error);
-    return [];
-  }
-}
-
-carrouselRouter.get('', async (req, res) => {
-  try {
-    const preSignedUrls = await getImagePreSignedUrls();
-    res.json(preSignedUrls);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
+carrouselRouter.get('', (req, res) => {
+    const params = {
+      Bucket: 'automation-pe-2-cumbucket',
+      Prefix: 'images/'
+    };
+  
+    s3.listObjectsV2(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        const imageUrls = data.Contents
+          .filter(item => item.Key !== 'images/') // Exclude the 'images/' prefix
+          .map(item => ({ url: `https://automation-pe-2-cumbucket.s3.amazonaws.com/${item.Key}` }));
+        res.json(imageUrls);
+      }
+    });
+  });
+  
 module.exports = carrouselRouter;
